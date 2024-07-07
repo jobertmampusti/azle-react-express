644a1637f7da975a10f92914457e60eae7481641
Jobert Mampusti <mampustijobert@gmail.com>
2024-07-07 21:50:29 +0800

    template upload

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <main className="font-kanit">
+      <div className="flex flex-col items-center">
+        <img src="/logo2.svg" alt="DFINITY logo" />
+        <p className="text-4xl mt-10">Welcome to Internet Computer!</p>
+        <p className="text-2xl mt-5">Build your dapps using this template, have fun!</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
